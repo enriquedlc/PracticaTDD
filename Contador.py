132332be1b054c605872fd05a218745229f126d6
Enrique <enriquedlc2d@gmail.com>
2022-06-10 10:31:13 +0100

    Test 2 verde

--- Contador.py
+++ Contador.py
@@ -1,6 +1,6 @@
 class Contador():
 
-    def __init__(self, inicial, incremento, limite):
+    def __init__(self, inicial = 0, incremento = 1, limite = None):
         self.inicial = inicial
         self.incremento = incremento
         self.limite = limite
