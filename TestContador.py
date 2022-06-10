978690dc7ed954e5e2886145361270334a1971fd
Enrique <enriquedlc2d@gmail.com>
2022-06-10 10:28:17 +0100

    Test 2 rojo

--- TestContador.py
+++ TestContador.py
@@ -16,5 +16,17 @@ class test_crear_constructor(unittest.TestCase): # heredamos de unittest de todo
         self.assertEqual(contador1.incremento, 2)
         self.assertEqual(contador1.limite, 5)
 
+
+    # Segundo test:  El valor inicial y el incremento tomarán un valor de 0 y 1 respectivamente si no se indica nada. 
+    # El  límite es necesario indicarlo siempre.  
+
+    def test2(self):
+        
+        contador2 = contador.Contador(limite = 3)
+
+        self.assertEqual(contador2.inicial, 0)
+        self.assertEqual(contador2.incremento, 1)
+        self.assertEqual(contador2.limite, 3)
+
 if __name__=="__main__":
     unittest.main()
